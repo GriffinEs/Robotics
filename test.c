5c2a6f0b4b3f3e5e196ef1448d748b66a9e36af5
GriffinEs <pokegriff@gmail.com>
2014-12-28 20:36:43 -0500

    My First Commit
    A c program compiled and put into exe form.

---
+++ test.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main(){
+	printf("Hello, world!\n");
+	printf("I committed this with git\nProgrammed in visual studio");
+	printf("it's cool\n\n\n\n");
+	printf("aaron get rekt\n\n:-)");
+	while (1){
+		// get shrekt
+	}
+	return 0;
+}
