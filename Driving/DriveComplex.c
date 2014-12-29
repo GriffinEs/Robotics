1de615fe22c4e0be4061a096cd976d97aeb288f0
GriffinEs <pokegriff@gmail.com>
2014-12-29 15:37:47 -0500

    Port Changes
    Changed the ports for the motors because the dual motors on the wheels
    must go in a 3 wire port (not ports 1 and 10)

--- Driving/DriveComplex.c
+++ Driving/DriveComplex.c
@@ -21,15 +21,15 @@ task killswitch(){ // The task to listen for the killswitch to be pressed and st
 }
 
 task openclaw(){				// the task to open the claw
-	motor[port5] = 127;		// make the claw motor open
+	motor[port7] = 127;		// make the claw motor open
 	wait1Msec(2000);			// wait for two seconds
-	motor[port5] = 0;			// stop opening the claw so that the motor won't get stripped
+	motor[port7] = 0;			// stop opening the claw so that the motor won't get stripped
 }
 
 task closeclaw(){				// the task to close the claw!
-	motor[port5] = -127;	// close the claw fast at first
+	motor[port7] = -127;	// close the claw fast at first
 	wait1msec(1500);			// for 1.5 seconds, and then
-	motor[port5] = -100;	// constantly close the claw to hold a cube at a lower power (no gear strip)
+	motor[port7] = -100;	// constantly close the claw to hold a cube at a lower power (no gear strip)
 }
 
 task main(){
@@ -38,17 +38,17 @@ task main(){
 	while(1){ // Do this forever, unless the task is stopped.
 
 		if(arcade){ 	//................................ the code that runs the joysticks is dependant on arcade mode.
-			motor[port1] = (vexRT[Ch1]-vexRT[Ch2]); 		// left wheel
-			motor[port10] = -(vexRT[Ch1]+vexRT[Ch2]); 	// right wheel
-			motor[port2] = -(vexRT[Ch3]); 							// left arms
-			motor[port3] = (vexRT[Ch3]); 								// right arms
+			motor[port2] = (vexRT[Ch1]-vexRT[Ch2]); 		// left wheel
+			motor[port3] = -(vexRT[Ch1]+vexRT[Ch2]); 	// right wheel
+			motor[port4] = -(vexRT[Ch3]); 							// left arms
+			motor[port5] = (vexRT[Ch3]); 								// right arms
 		} else { 			//................................ if it's not in arcade mode...
-			motor[port1] = vexRT[Ch3];									// left wheel to left joystick
-			motor[port10] = vexRT[Ch2];									// right wheel to right joystick
-			motor[port2] = (vexRT[Btn6U]-vexRT[Btn5D])*127;			// left arms to a button
-			motor[port3] = -(vexRT[Btn6U]-vexRT[Btn5D])*127;		// right arms to a button
+			motor[port2] = vexRT[Ch3];									// left wheel to left joystick
+			motor[port3] = vexRT[Ch2];									// right wheel to right joystick
+			motor[port4] = (vexRT[Btn6U]-vexRT[Btn5D])*127;			// left arms to a button
+			motor[port5] = -(vexRT[Btn6U]-vexRT[Btn5D])*127;		// right arms to a button
 		} 						//........................................ the buttons below don't change.
-		motor[port4] = -(vexRT[Btn5U]*127-vexRT[Btn5D]*127); 	// claw actuator
+		motor[port6] = -(vexRT[Btn5U]*127-vexRT[Btn5D]*127); 	// claw actuator
 
 		if(vexRT[Btn7L] && !vexRT[Btn7R]){					// if 7l is pressed and 7r is not
 			stopTask(openclaw);												// stop trying to open the claw
@@ -61,6 +61,6 @@ task main(){
 		}
 
 		ArcadeFlip(vexRT[Btn8D], oldbtn);	// Function to switch to and from arcade mode
-		oldbtn = vexRT[Btn8D]; 						// Set the value of l to the current vexRT[Btn8D] so it can be compared to the one
-	}																// on the next cycle.
+		oldbtn = vexRT[Btn8D]; 						// Set the value of oldbtn to the current vexRT[Btn8D] so it can be compared to the one
+	}																		// on the next cycle.
 }
