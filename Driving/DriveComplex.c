e3608bcba1dd0de3b045f92323506ff5d0009949
GriffinEs <pokegriff@gmail.com>
2014-12-29 14:36:00 -0500

    Claw Work
    Removed useless code due to previous changes, and added a better system
    for the claw (no longer have to hold down button).

--- Driving/DriveComplex.c
+++ Driving/DriveComplex.c
@@ -20,6 +20,18 @@ task killswitch(){ // The task to listen for the killswitch to be pressed and st
 	}
 }
 
+task openclaw(){				// the task to open the claw
+	motor[port5] = 127;		// make the claw motor open
+	wait1Msec(2000);			// wait for two seconds
+	motor[port5] = 0;			// stop opening the claw so that the motor won't get stripped
+}
+
+task closeclaw(){				// the task to close the claw!
+	motor[port5] = -127;	// close the claw fast at first
+	wait1msec(1500);			// for 1.5 seconds, and then
+	motor[port5] = -100;	// constantly close the claw to hold a cube at a lower power (no gear strip)
+}
+
 task main(){
 	startTask(killswitch); // Start the task to listen for killswitch press
 
@@ -37,17 +49,18 @@ task main(){
 			motor[port3] = -(vexRT[Btn6U]-vexRT[Btn5D])*127;		// right arms to a button
 		} 						//........................................ the buttons below don't change.
 		motor[port4] = -(vexRT[Btn5U]*127-vexRT[Btn5D]*127); 	// claw actuator
-		motor[port5] = (vexRT[Btn7L]*127-vexRT[Btn7R]*127); 	// the claaaaw
+
+		if(vexRT[Btn7L] && !vexRT[Btn7R]){					// if 7l is pressed and 7r is not
+			stopTask(openclaw);												// stop trying to open the claw
+			stopTask(closeclaw);											// also kill previous instances of this task
+			startTask(closeclaw);											// and start closing it.
+		} else if(vexRT[Btn7R] && !vexRT[Btn7L]){ 	// otherwise, if 7r is on and 7l isn't..
+			stopTask(closeclaw);											// stop the closeclaw task
+			stopTask(openclaw);												// also kill previous instances of this task
+			startTask(openclaw);											// then start a new openclaw task
+		}
 
 		ArcadeFlip(vexRT[Btn8D], oldbtn);	// Function to switch to and from arcade mode
 		oldbtn = vexRT[Btn8D]; 						// Set the value of l to the current vexRT[Btn8D] so it can be compared to the one
 	}																// on the next cycle.
-
-	motor[port2] = 0; // if the kill switch is pressed, STOP EVERYTHING
-	motor[port9] = 0;
-	motor[port4] = 0;
-	motor[port5] = 0;
-	motor[port6] = 0;
-	motor[port7] = 0;
-	motor[port8] = 0;
 }
